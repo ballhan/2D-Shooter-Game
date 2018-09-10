68fa86ac2d9a2fa7303e10d1b928ac18911c6aae
Bohan Chen <bohanchen81@yahoo.com>
2018-09-10 13:44:19 -0400

    Add files via upload

--- Js/game.js
+++ Js/game.js
@@ -1,8 +1,30 @@
-var canvas = document.querySelector('canvas');
-var c = canvas.getContext('2d');
+var myGamePiece;
 
-canvas.width = 500;
-canvas.height = 500;
+function startGame() {
+    myGameArea.start();
+    myGamePiece = new component(30, 30, "red", 235, 400);
+}
+
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 500;
+        this.canvas.height = 500;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 10);
+        window.addEventListener('keydown', function (e) {
+            myGameArea.keys = (myGameArea.keys || []);
+            myGameArea.keys[e.keyCode] = (e.type == "keydown");
+        })
+        window.addEventListener('keyup', function (e) {
+            myGameArea.keys[e.keyCode] = (e.type == "keydown");            
+        })
+    }, 
+    clear : function(){
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
 
 function component(width, height, color, x, y) {
     this.gamearea = myGameArea;
@@ -27,11 +49,10 @@ function updateGameArea() {
     myGameArea.clear();
     myGamePiece.speedX = 0;
     myGamePiece.speedY = 0;    
-    if (myGameArea.keys && myGameArea.keys[37]) {myGamePiece.speedX = -1; }
-    if (myGameArea.keys && myGameArea.keys[39]) {myGamePiece.speedX = 1; }
-    if (myGameArea.keys && myGameArea.keys[38]) {myGamePiece.speedY = -1; }
-    if (myGameArea.keys && myGameArea.keys[40]) {myGamePiece.speedY = 1; }
-    myGamePiece.newPos();
-    myGamePiece.checkWin();    
+    if (myGameArea.keys && myGameArea.keys[37] && myGamePiece.x > 0) {myGamePiece.speedX = -1; }
+    if (myGameArea.keys && myGameArea.keys[39] && myGamePiece.x < 470) {myGamePiece.speedX = 1; }
+    if (myGameArea.keys && myGameArea.keys[38] && myGamePiece.y > 0) {myGamePiece.speedY = -1; }
+    if (myGameArea.keys && myGameArea.keys[40] && myGamePiece.y < 470) {myGamePiece.speedY = 1; }
+    myGamePiece.newPos();    
     myGamePiece.update();
 }
