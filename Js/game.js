24501c1a05740b249348b91a2fe2e2a1ca71eaca
Bohan Chen <bohanchen81@yahoo.com>
2018-09-20 12:25:12 -0400

    Update game.js

--- Js/game.js
+++ Js/game.js
@@ -361,6 +361,7 @@ function enemy(player) {
         playerY1 = this.player.y;
         playerY2 = this.player.y + this.player.bodyHeight;
         if (enemyX1 < playerX2 && enemyX2 > playerX1 && enemyY1 < playerY2 && enemyY2 > playerY1) {
+            alert("GAME OVER");
             document.location.reload();
         }
     }
